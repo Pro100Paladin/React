2cba3ff364c0ae66d61ad08a9b4b1a6066f6b425
Pro100Paladin <pro100paladin@icloud.com>
2025-02-15 17:04:57 +0300

    hw1

--- hw/hw1/src/App.jsx
+++ hw/hw1/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import './Message.css'
 import Message from './Message'
 
 function App() {
 
   return (
       <div className='App'>
-        <Message namePerson={"Djon"} yearPerson={37} />
-        <Message namePerson={"Oksana"} yearPerson={35} propsBoollean = "true" />
+        <Message namePerson={"Djon"} yearPerson={37} propsBoollean={true}/>
+        <Message namePerson={"Oksana"} yearPerson={35} propsBoollean = {false} />
       </div>
   )
 }
