f63e3f35e0ae6ed8369737058fd31e6214c4135e
Pro100Paladin <pro100paladin@icloud.com>
2025-02-15 12:47:45 +0300

    first commit

---
+++ sem/sem1app/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import './task1/greeting.css'
+
+import Greeting from './task1/Greeting'
+import NewComponent from './task1/NewComponent'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <Greeting />
+        <NewComponent />
+      </div>
+    </>
+  )
+}
+
+
+export default App
+
---
+++ sem/sem1app/src/task1/NewComponent.jsx
@@ -0,0 +1,10 @@
+
+function ButtonComponent() {
+    return ( 
+        <div>
+            <button className="Greeting__button">Принять сообщение</button>
+        </div>
+     );
+}
+
+export default ButtonComponent;
